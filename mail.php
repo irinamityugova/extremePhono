7be2b9dd28b54b4d0600451b9067c1c7062c6abe
Irina <irishka2863@yahoo.com>
2018-08-15 14:08:26 -0700

    tRY get

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
-if(isset( $_POST['name']))
-  $name = $_POST['name'];
-if(isset( $_POST['email']))
-  $email = $_POST['email'];
-if(isset( $_POST['message']))
-  $message = $_POST['message'];
-if(isset( $_POST['subject']))
-  $subject = $_POST['subject'];
+if(isset( $_GET['name']))
+  $name = $_GET['name'];
+if(isset( $_GET['email']))
+  $email = $_GET['email'];
+if(isset( $_GET['message']))
+  $message = $_GET['message'];
+if(isset( $_GET['subject']))
+  $subject = $_GET['subject'];
 
 $content="From: $name \n Email: $email \n Message: $message";
 $recipient = "info@irinamityugova.me";
